30ae184daad4cdd38e379aad9987d9e705e2f952
Kunal Nirban <kunalydv@proton.me>
2025-09-19 14:17:52 +0530

    Update tokenizer.java

--- String/tokenizer.java
+++ String/tokenizer.java
@@ -8,3 +8,10 @@ public class tokenizer {
         System.out.println(st.nextToken());
     }
 }
+
+/**
+ * StringTokenizer is a legacy class in Java (java.util package) used to split a string into tokens.
+ * Tokens are substrings separated by delimiters (like space, comma, etc.).
+ * Each call to nextToken() returns the next token in the string.
+ * Note: Since Java 1.4, String.split() and regex are recommended, but StringTokenizer is still widely used for simple tokenizing tasks.
+ */
